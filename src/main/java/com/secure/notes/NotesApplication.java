ad8e11a5aeff138254bc73bed96c28124abe5442
Shaheen <Shaheen806@gmail.com>
2025-03-06 00:38:29 +1100

    unused code

--- src/main/java/com/secure/notes/NotesApplication.java
+++ src/main/java/com/secure/notes/NotesApplication.java
@@ -2,10 +2,8 @@ package com.secure.notes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-
 public class NotesApplication {
 
 	public static void main(String[] args) {
